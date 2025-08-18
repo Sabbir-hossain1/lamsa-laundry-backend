7fda636ef86a3ec1b45b01ea39d95d6405469ab6
Sabbir-hossain1 <sabbir.cse.duet@gmail.com>
2025-08-18 15:59:37 +0600

    feat: simple jwt added in project

--- ll/settings.py
+++ ll/settings.py
@@ -54,6 +54,9 @@ MIDDLEWARE = [
 
 REST_FRAMEWORK = {
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ),
 }
 
 
