1d1fb83b74f75caef25d6d77ff7a0abfdf3e7339
Sabbir-hossain1 <sabbir.cse.duet@gmail.com>
2025-08-18 22:37:45 +0600

    feat: service app and models added

---
+++ service/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from service.models.service_model import Service
+
+admin.site.register(Service)
---
+++ service/models/service_model.py
@@ -0,0 +1,19 @@
+from common_bases.base_model import BaseModel
+from django.db import models
+
+
+class Service(BaseModel):
+    title = models.CharField(max_length=255)
+    primary_image = models.ImageField(
+        upload_to="services/primary/", blank=True, null=True
+    )
+    secondary_image = models.ImageField(
+        upload_to="services/secondary/", blank=True, null=True
+    )
+
+    class Meta:
+        verbose_name = "Service"
+        verbose_name_plural = "Services"
+
+    def __str__(self):
+        return self.title
