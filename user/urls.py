2f067b6097785809623c587c0b16a4765325758f
Sabbir-hossain1 <sabbir.cse.duet@gmail.com>
2025-08-18 22:00:47 +0600

    feat: auth urls added in user app

--- user/urls.py
+++ user/urls.py
@@ -1,7 +1,7 @@
 from rest_framework.routers import DefaultRouter
-from user.views.user_model_viewset import CustomUserViewSet
+from user.views.user_model_viewset import CustomUserViewSet, AuthViewSets
 
 router = DefaultRouter()
+router.register(r"auth", AuthViewSets, basename="auth"),
 router.register(r"users", CustomUserViewSet, basename="user")
-
 urlpatterns = router.urls
