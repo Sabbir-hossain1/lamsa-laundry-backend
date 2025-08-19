8c2bd19ab91d6c3b303faf33d4af3a7cf7ca8d82
Sabbir-hossain1 <sabbir.cse.duet@gmail.com>
2025-08-19 11:34:34 +0600

    feat: product model viewset added

---
+++ service/views/product_model_viewset.py
@@ -0,0 +1,39 @@
+from rest_framework import viewsets, permissions
+from service.models.product_model import Product
+from service.serializers.product_model_serializers import (
+    ProductListSerializer,
+    ProductDetailSerializer,
+    ProductCreateUpdateSerializer,
+)
+
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+
+    def get_queryset(self):
+        """
+        Customize queryset per action.
+        Example: list only latest products.
+        """
+        qs = Product.objects.all()
+        if self.action == "list":
+            return qs.order_by("-created_at")
+        return qs
+
+    def get_permissions(self):
+        """
+        Control permissions per action.
+        """
+        if self.action in ["list", "retrieve"]:
+            return [permissions.AllowAny()]  # Public access
+        return [permissions.IsAuthenticated()]  # Only logged-in users can modify
+
+    def get_serializer_class(self):
+        """
+        Use different serializer depending on action.
+        """
+        if self.action == "list":
+            return ProductListSerializer
+        elif self.action == "retrieve":
+            return ProductDetailSerializer
+        return ProductCreateUpdateSerializer
