46fcfe2ddff2194d55aa53c0100ca73b00060ba7
Sabbir-hossain1 <sabbir.cse.duet@gmail.com>
2025-08-19 11:34:02 +0600

    feat: products urls added in parent urls

--- service/urls.py
+++ service/urls.py
@@ -1,7 +1,9 @@
 from rest_framework.routers import DefaultRouter
 from service.views.serive_modelviewset import ServiceViewSet
+from service.views.product_model_viewset import ProductViewSet
 
 router = DefaultRouter()
 router.register("services", ServiceViewSet, basename="service")
+router.register("products", ProductViewSet, basename="product")
 
 urlpatterns = router.urls
