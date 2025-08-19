dc6defd0b683cbce03f7db382b003e6cef90f91a
Sabbir-hossain1 <sabbir.cse.duet@gmail.com>
2025-08-19 11:19:07 +0600

    feat: product model added in admin register

--- service/admin.py
+++ service/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from service.models.service_model import Service
+from service.models.product_model import Product
 from user.models import Notification
 
 admin.site.register(Service)
 admin.site.register(Notification)
+admin.site.register(Product)
