5ede08964f25d568268479a417fe62fbedd6c748
Sabbir-hossain1 <sabbir.cse.duet@gmail.com>
2025-08-19 22:06:37 +0600

    feat: Schedule model register in admin.py

--- order/admin.py
+++ order/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from order.models.order_model import Order
 from order.models.order_item_model import OrderItem
+from order.models.schedule_model import Schedule
 
 # Register your models here.
 admin.site.register(Order)
 admin.site.register(OrderItem)
+admin.site.register(Schedule)
