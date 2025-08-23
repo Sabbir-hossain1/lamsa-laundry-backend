c47e65a1bb04f6f4a10f99ddf8028932ba953e35
Sabbir-hossain1 <sabbir.cse.duet@gmail.com>
2025-08-23 22:44:06 +0600

    feat: order item urls added

--- order/urls.py
+++ order/urls.py
@@ -1,9 +1,9 @@
 from rest_framework.routers import DefaultRouter
 from order.views.order_model_viewset import OrderViewSet
-from order.views.order_item_model_viewset import OrderItemModelViewset
+from order.views.order_item_model_viewset import OrderItemModelViewSet
 
 router = DefaultRouter()
 router.register("orders", OrderViewSet, basename="order")
-router.register("order-items", OrderItemModelViewset, basename="order-items")
+router.register("order-items", OrderItemModelViewSet, basename="order-items")
 
 urlpatterns = router.urls
