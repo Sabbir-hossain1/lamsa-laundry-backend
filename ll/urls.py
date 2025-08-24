f9fc3ce6044cb69a03268fc92fe4c03c5f13d1b1
hossainchisty <hossain.chisty11@gmail.com>
2025-08-24 08:57:03 +0600

    feat: reorganize URL configuration and enhance admin site customization

--- ll/urls.py
+++ ll/urls.py
@@ -15,14 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
 from drf_spectacular.views import (
     SpectacularAPIView,
     SpectacularSwaggerView,
-    SpectacularRedocView,
 )
-
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -36,3 +36,13 @@ urlpatterns = [
         name="swagger-ui",
     ),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+# Customization `header`, `site-title`, and `index`
+admin.site.site_header = "SOS e-Learning App"
+admin.site.site_title = "Softmax online school"
+admin.site.index_title = "Welcome to Softmax online school"
