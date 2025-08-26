77ca576eacd6df22cd10463c7267052cfe27a57f
Sabbir-hossain1 <sabbir.cse.duet@gmail.com>
2025-08-26 22:51:22 +0600

    feat: refactor service serializers to include AdminServiceDetailSerializer and ServiceDropdownSerializer

--- service/serializers/service_model_serializers.py
+++ service/serializers/service_model_serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from service.models.service_model import Service
+from service.serializers.price_model_serializers import PriceListSerializer
 
 
 class ServiceListSerializer(serializers.ModelSerializer):
@@ -10,12 +11,27 @@ class ServiceListSerializer(serializers.ModelSerializer):
         fields = ["id", "title", "primary_image"]
 
 
-class ServiceDetailSerializer(serializers.ModelSerializer):
+class AdminServiceDetailSerializer(serializers.ModelSerializer):
     """For retrieving single service details."""
 
     class Meta:
         model = Service
         fields = ["id", "title", "primary_image", "secondary_image"]
+
+
+class ServiceDetailSerializer(serializers.ModelSerializer):
+    """For retrieving single service details."""
+
+    products = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Service
+        fields = ["id", "title", "secondary_image", "products"]
+
+    def get_products(self, obj):
+        prices = obj.prices
+        serializer = PriceListSerializer(prices, many=True)
+        return serializer.data
 
 
 class ServiceCreateUpdateSerializer(serializers.ModelSerializer):
@@ -24,3 +40,13 @@ class ServiceCreateUpdateSerializer(serializers.ModelSerializer):
     class Meta:
         model = Service
         fields = ["title", "primary_image", "secondary_image"]
+
+
+class ServiceDropdownSerializer(serializers.ModelSerializer):
+
+    label = serializers.CharField(source="title")
+    value = serializers.IntegerField(source="id")
+
+    class Meta:
+        model = Service
+        fields = ["label", "value"]
