70d4fcb6aba63b1214f61778ec6c8069989c5a86
Sabbir-hossain1 <sabbir.cse.duet@gmail.com>
2025-08-26 22:51:13 +0600

    feat: update price serializers to use dropdown serializers for service and product

--- service/serializers/price_model_serializers.py
+++ service/serializers/price_model_serializers.py
@@ -1,16 +1,38 @@
 from rest_framework import serializers
 from service.models.price_model import Price
+from service.models.product_model import Product
+from service.models.service_model import Service
+
+
+class ServiceDropdownSerializer(serializers.ModelSerializer):
+
+    label = serializers.CharField(source="title")
+    value = serializers.IntegerField(source="id")
+
+    class Meta:
+        model = Service
+        fields = ["label", "value"]
+
+
+class ProductDropdownSerializer(serializers.ModelSerializer):
+
+    label = serializers.CharField(source="name")
+    value = serializers.IntegerField(source="id")
+
+    class Meta:
+        model = Product
+        fields = ["label", "value"]
 
 
 class PriceListSerializer(serializers.ModelSerializer):
     """For listing prices (lightweight)."""
 
-    service = serializers.StringRelatedField()
-    product = serializers.StringRelatedField()
+    service = ServiceDropdownSerializer()
+    product = ProductDropdownSerializer()
 
     class Meta:
         model = Price
-        fields = ["id", "service", "product", "price", "sell_price"]
+        fields = ["id", "service", "product", "sell_price"]
 
 
 class PriceDetailSerializer(serializers.ModelSerializer):
