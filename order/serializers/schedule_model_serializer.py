388748e947ba5cec35a27d656a8a3bab4b63bf6f
Sabbir-hossain1 <sabbir.cse.duet@gmail.com>
2025-09-08 11:42:06 +0600

    feat: schedule model serializer added

---
+++ order/serializers/schedule_model_serializer.py
@@ -0,0 +1,9 @@
+from rest_framework.serializers import ModelSerializer
+from order.models.schedule_model import Schedule
+
+
+class ScheduleModelSerializer(ModelSerializer):
+    class Meta:
+        model = Schedule
+        fields = "__all__"
+        read_only_fields = ["id", "created_at", "updated_at"]
