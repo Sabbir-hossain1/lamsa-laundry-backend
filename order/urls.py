0962be01d7571a44d217bc67c4cd04c2eb23450a
Sabbir-hossain1 <sabbir.cse.duet@gmail.com>
2025-09-08 12:32:14 +0600

    feat: schedule model viewset urls added

--- order/urls.py
+++ order/urls.py
@@ -1,9 +1,11 @@
 from rest_framework.routers import DefaultRouter
 from order.views.order_model_viewset import OrderViewSet
 from order.views.order_item_model_viewset import OrderItemModelViewSet
+from order.views.schedule_model_viewset import ScheduleModelViewSet
 
 router = DefaultRouter()
 router.register("orders", OrderViewSet, basename="order")
 router.register("order-items", OrderItemModelViewSet, basename="order-items")
+router.register("schedules", ScheduleModelViewSet, basename="schedules")
 
 urlpatterns = router.urls
