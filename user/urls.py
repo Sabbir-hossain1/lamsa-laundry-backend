6bee0a3d943472db9d6a65c5b82a819e4e7e96bc
Sabbir-hossain1 <sabbir.cse.duet@gmail.com>
2025-09-21 10:04:53 +0600

    feat: address urls added

--- user/urls.py
+++ user/urls.py
@@ -1,7 +1,15 @@
 from rest_framework.routers import DefaultRouter
 from user.views.user_model_viewset import CustomUserViewSet, AuthViewSets
+from rest_framework_simplejwt.views import TokenRefreshView
+from user.views.address_model_viewset import AddressViewSet
+
+from django.urls import path
 
 router = DefaultRouter()
 router.register(r"auth", AuthViewSets, basename="auth"),
 router.register(r"users", CustomUserViewSet, basename="user")
-urlpatterns = router.urls
+router.register(r"address", AddressViewSet, basename="address")
+
+urlpatterns = [
+    path("auth/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+] + router.urls
