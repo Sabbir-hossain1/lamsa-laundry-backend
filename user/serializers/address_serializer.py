a9f16314fc64fe056be704a354c9e9d286e721f1
Sabbir-hossain1 <sabbir.cse.duet@gmail.com>
2025-09-21 10:05:11 +0600

    feat: address serializer added

---
+++ user/serializers/address_serializer.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+from user.models import Address
+
+
+class AddressSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Address
+        fields = "__all__"
+        read_only_fields = ["created_at", "updated_at"]
